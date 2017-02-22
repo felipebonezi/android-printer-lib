a18e3a71507aac019aae401097d89fe88db05eae
felipebonezi <felipebonezi@gmail.com>
2017-02-22 09:39:06 -0300

    - Adjusting sample.

--- app/src/main/java/br/com/ehureka/printers/flow/AppActivity.java
+++ app/src/main/java/br/com/ehureka/printers/flow/AppActivity.java
@@ -31,59 +31,24 @@ public class AppActivity extends Activity implements OnPrinterListener {
         int width = printer.getWidth();
 
         printSeparator(printer, width);
-        printlnDoubleEmphasized(printer, helper.center("OCS", width));
-        printer.println(helper.center("LOTERIAS ONLINE", width));
+        printlnDoubleEmphasized(printer, helper.center("Olá, meu caro!", width));
+        printer.println(helper.center("Tudo bem?", width));
         printSeparator(printer, width);
 
-        printlnDoubleEmphasized(printer, helper.center("FEDERAL - 18:00h de 08/02/2017", width));
+        printlnDoubleEmphasized(printer, helper.center("Data - 18:00h de 08/02/2017", width));
         printSeparator(printer, width);
 
-        printer.println(helper.left("PDV.....: 004.351.1", width));
-        printer.println(helper.left("Cambista: PDVL 004", width));
+        printer.println(helper.left("Exemplo#1: 004.351.1", width));
+        printer.println(helper.left("Exemplo#2: PDVL 004", width));
         printSeparator(printer, width);
 
-        printlnDoubleEmphasized(printer, helper.center("BILHETE N.00043", width));
+        printlnDoubleEmphasized(printer, helper.center("IMPRESSÃO N.00043", width));
         printSeparator(printer, width);
 
         printer.println(helper.center("IMPRESSO ÀS 10:38h DE 08/02/2017", width));
 
         printSeparator(printer, width);
-        printer.println(helper.left("Centena Invertida", width));
-        printer.println(helper.left("3.1.5", width));
-        printer.println(helper.left("3.1.4", width));
-        printer.println(helper.left("1.5.1", width));
-        printer.println(helper.left("9.6.4", width));
-        printer.println(helper.left("à R$0,50 no 1. prêmio", width));
-        printer.println(helper.right("Sub-total: R$12,00", width));
-
-        printSeparator(printer, width);
-        printer.println(helper.left("Milhar", width));
-        printer.println(helper.left("3151", width));
-        printer.println(helper.left("9614", width));
-        printer.println(helper.left("à R$0,25 no 1., 2. prêmio", width));
-        printer.println(helper.right("Sub-total: R$20,00", width));
-
-        printSeparator(printer, width);
-        printer.println(helper.left("Centena", width));
-        printer.println(helper.left("110", width));
-        printer.println(helper.left("314", width));
-        printer.println(helper.left("151", width));
-        printer.println(helper.left("964", width));
-        printer.println(helper.left("à R$0,01 no 1. prêmio", width));
-        printer.println(helper.right("Sub-total: R$1,00", width));
-
-        printSeparator(printer, width);
-        printDoubleEmphasized(printer, helper.right("Total: R$33,00", width));
-
-        printSeparator(printer, width);
-        printer.println(helper.center("B O A  S O R T E !", width));
-        printer.println();
-        printer.println(helper.center("VÁLIDO POR 30 DIAS", width));
-
-        printSeparator(printer, width);
-        printer.println("Milhares 1308, 1313, 1720, 1820, 1873, 1920, 1965, 2020 estão cotadas em 20%.");
-        printer.println();
-        printer.println("Milhares 1973, 2933 estão cotadas em 50%.");
+        printer.println(helper.center("AJUDE A COMUNIDADE!", width));
 
         printSeparator(printer, width);
         String barcode = "000430043511218001702080";
